7ab9c1e3398ee6a780bbc6c8db0aee830ddbc291
Dima Hrykalovsky <vvhoijarvis@gmail.com>
2019-11-05 02:11:41 +0200

    git push

---
+++ main.js
@@ -0,0 +1,18 @@
+(function () {
+    const burger1 = document.querySelector('.burger1');
+    burger1.addEventListener('click', () => {
+        burger1.classList.toggle('burger_active1');
+    });
+}());
+(function () {
+    const burger2 = document.querySelector('.burger2');
+    burger2.addEventListener('click', () => {
+        burger2.classList.toggle('burger_active2');
+    });
+}());
+(function () {
+    const burger3 = document.querySelector('.burger3');
+    burger3.addEventListener('click', () => {
+        burger3.classList.toggle('burger_active3');
+    });
+}());
